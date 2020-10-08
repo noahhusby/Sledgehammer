a235c090e307201d9f5d5fb9bc989968df509d7a
Noah Husby <noahhusby@gmail.com>
2020-10-08 08:40:55 -0400

    Fixed copyrights part 2

--- sledgehammer-bungeecord/src/main/java/com/noahhusby/sledgehammer/commands/fragments/admin/PermissionCheckFragment.java
+++ sledgehammer-bungeecord/src/main/java/com/noahhusby/sledgehammer/commands/fragments/admin/PermissionCheckFragment.java
@@ -14,7 +14,6 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with Sledgehammer.  If not, see <https://github.com/noahhusby/Sledgehammer/blob/master/LICENSE/>.
- *
  */
 
 package com.noahhusby.sledgehammer.commands.fragments.admin;
--- sledgehammer-bungeecord/src/main/java/net/minecraftforge/common/config/IFieldWrapper.java
+++ sledgehammer-bungeecord/src/main/java/net/minecraftforge/common/config/IFieldWrapper.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2020 Noah Husby
- * Sledgehammer [Bungeecord] - IFieldWrapper.java
+ * Minecraft Forge
+ * Copyright (c) 2016-2020.
  *
- * Sledgehammer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
  *
- * Sledgehammer is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with Sledgehammer.  If not, see <https://github.com/noahhusby/Sledgehammer/blob/master/LICENSE/>.
- *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 package net.minecraftforge.common.config;
