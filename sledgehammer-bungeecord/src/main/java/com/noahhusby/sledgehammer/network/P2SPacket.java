28e7802480806a9460a1e685cb01605d2fb927ce
Noah Husby <noahhusby@gmail.com>
2020-10-08 08:38:29 -0400

    Updated copyright

--- sledgehammer-bungeecord/src/main/java/com/noahhusby/sledgehammer/network/P2SPacket.java
+++ sledgehammer-bungeecord/src/main/java/com/noahhusby/sledgehammer/network/P2SPacket.java
@@ -12,8 +12,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Sledgehammer.  If not, see <https://github.com/noahhusby/Sledgehammer/blob/master/LICENSE/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with Sledgehammer.  If not, see <https://github.com/noahhusby/Sledgehammer/blob/master/LICENSE/>.
+ *
  */
 
 package com.noahhusby.sledgehammer.network;
