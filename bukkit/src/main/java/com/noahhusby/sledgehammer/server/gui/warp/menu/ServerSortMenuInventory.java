6eb1793d8101ca2e66eaac5927b976171132eb18
Noah Husby <noahhusby@gmail.com>
2022-01-13 11:20:06 -0500

    Updated copyright

--- bukkit/src/main/java/com/noahhusby/sledgehammer/server/gui/warp/menu/ServerSortMenuInventory.java
+++ bukkit/src/main/java/com/noahhusby/sledgehammer/server/gui/warp/menu/ServerSortMenuInventory.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright 2020-2021 noahhusby
+ * Copyright 2020-2022 noahhusby
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
